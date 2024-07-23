cc683dde315ed9cb9ba5620ac6ef678af5a5f4d6
Aditya <lapratomo24@yahoo.com>
2024-07-23 14:21:48 +0200

    add starting code

---
+++ main.py
@@ -0,0 +1,31 @@
+# Calculator
+
+logo = '''  _            _       _             
+          | |          | |     | |            
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ 
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |   
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
+'''
+
+# addition
+def add(a, b):
+    return a + b
+
+# subtraction
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
